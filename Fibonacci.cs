6d7c755f9cea8abb0721b2a403099cd2f5ae7ee5
central1214 <81129919+central1214@users.noreply.github.com>
2021-03-30 22:02:27 +0900

    피보나치수열,묵찌빠

---
+++ Fibonacci.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        class Fibonacci
+        {
+            public void Fibo1(int num)
+            {
+                long x = 1, y = 0;
+                long fibonacci = 0;
+
+
+                for (int i = 0; i < num; i++)
+                {
+                    y = x;
+                    x = fibonacci;
+                    fibonacci = x + y;
+                    Console.WriteLine(i + 1 + "번째 Fibonacci 수열은 :" + fibonacci);
+                }
+
+            }
+            public long Fibo2(int num)
+            {
+                if (num == 0) { return 0; }
+                if (num == 1) { return 1; }
+                return Fibo2(num - 1) + Fibo2(num - 2);
+            }
+            // private static Dictionary<int, long> memo = new Dictionary<int, long>();
+
+            public long Fibo3(int num)
+            {
+                long[] fibo = new long[100];
+                fibo[0] = 0;
+                fibo[1] = 1;
+                int i;
+
+                for (i = 0; i < num; i++)
+                {
+                    fibo[i + 2] = fibo[i + 1] + fibo[i];
+                }
+                return fibo[i];
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Fibonacci fibo = new Fibonacci();
+            int num = int.Parse(Console.ReadLine());
+            fibo.Fibo1(num);
+            //Console.WriteLine(fibo.Fibo2(60));
+            Console.WriteLine(fibo.Fibo3(40));
+        }
+    }
+}
---
+++ mookjji.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace mookjji
+{
+    class MookJji
+    {
+        int a = 0;
+        int win, lose, draw;
+        public void Random(int a)
+        {
+            Random rand = new Random();
+            win = rand.Next(1, 4);
+            do
+            {
+                lose = rand.Next(1, 4);
+            } while (lose != win);
+
+            do
+            {
+                draw = rand.Next(1, 4);
+            } while (draw != win && draw != lose);
+            if (a == win)
+            { Console.WriteLine("승리!");}
+            if (a == lose)
+            { Console.WriteLine("패배!"); }
+            if (a == draw)
+            { Console.WriteLine("무승부!"); }
+
+
+        }
+      
+    class Program
+        {
+            static void Main(string[] args)
+            {
+                MookJji jji = new MookJji();
+
+                int mook = int.Parse(Console.ReadLine());
+                jji.Random(mook);
+               
+            }
+        }
+    }
+}
